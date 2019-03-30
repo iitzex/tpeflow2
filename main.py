08b0afdcad2dbc4e9cc99f3bb068cb77da7d794d
QN <iitzex@gmail.com>
2019-03-30 20:30:56 +0800

    settup for heroku

--- main.py
+++ main.py
@@ -206,5 +206,10 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
-    # app.run(debug=True)
+    debug = False
+    if os.environ.get('PORT') is None:
+        debug = True
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug)
+
